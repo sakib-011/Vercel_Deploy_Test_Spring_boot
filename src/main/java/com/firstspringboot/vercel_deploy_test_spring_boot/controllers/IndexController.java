6efaa0e35c7201db121370f2fcd01a85a49db83d
sakib-011 <sakibshourov001@email.com>
2026-07-15 14:21:07 +0600

    fix path

--- src/main/java/com/firstspringboot/vercel_deploy_test_spring_boot/controllers/IndexController.java
+++ src/main/java/com/firstspringboot/vercel_deploy_test_spring_boot/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String indexController(){
         return "index";
     }
